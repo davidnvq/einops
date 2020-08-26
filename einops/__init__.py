35b00f81f36c743c6b5be9c10dcd2317a1dc6869
Quang Nguyen <quanguet@gmail.com>
2020-08-26 18:02:36 +0900

    Add redim, concat, transpose

--- einops/__init__.py
+++ einops/__init__.py
@@ -1,5 +1,6 @@
 __author__ = 'Alex Rogozhnikov'
-__version__ = '0.2.0'
-__all__ = ['rearrange', 'reduce', 'parse_shape', 'asnumpy', 'EinopsError']
+__version__ = '0.2.1'
+__all__ = ['rearrange', 'reduce', 'parse_shape', 'asnumpy', 'EinopsError', 'redim', 'concat', 'transpose']
 
 from .einops import rearrange, reduce, repeat, parse_shape, asnumpy, EinopsError
+from .einops import redim, concat, transpose
